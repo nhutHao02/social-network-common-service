7e71557a8389c7b8bda852adddeda119dc44721d
nhutHao02 <trannhuthao091@gmail.com>
2024-11-02 15:08:26 +0700

    ud

--- validation/validation.go
+++ validation/validation.go
@@ -1,19 +1,43 @@
 package validation
 
 import (
+	"encoding/json"
 	"net/http"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
+	"github.com/nhutHao02/social-network-common-service/model"
+	constants "github.com/nhutHao02/social-network-common-service/utils/constanst"
 )
 
+func checkErr(c *gin.Context, err error) {
+
+}
 func CheckErrorType(c *gin.Context, err error) {
+	switch err.(type) {
+	case *json.UnmarshalTypeError:
+		c.JSON(http.StatusBadRequest, model.NewErrorResponse(err, constants.RequestInvalid))
+		return
+	case *json.SyntaxError:
+		c.JSON(http.StatusBadRequest, model.NewErrorResponse(err, constants.RequestInvalid))
+		return
+	case validator.ValidationErrors:
+		HandleValidationErrors(c, err)
+		return
+	default:
+		c.JSON(http.StatusBadRequest, model.NewErrorResponse(err, constants.RequestInvalid))
+		return
+	}
+
+}
+func HandleValidationErrors(c *gin.Context, err error) {
 	if validationErrs, ok := err.(validator.ValidationErrors); ok {
 		var messages []string
 		for _, fieldError := range validationErrs {
-			messages = append(messages, fieldError.Error()) // Thông báo lỗi từ tag validator
+			messages = append(messages, fieldError.Error())
 		}
-		c.JSON(http.StatusBadRequest, gin.H{"errors": messages})
+		c.JSON(http.StatusBadRequest, model.NewErrorResponse(err, strings.Join(messages, ",")))
 		return
 	}
 }
