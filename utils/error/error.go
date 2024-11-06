b9849ed91b42fe7f6bbee2ec61f723ef6fcbbe36
nhutHao02 <trannhuthao091@gmail.com>
2024-11-06 21:54:56 +0700

    update

--- utils/error/error.go
+++ utils/error/error.go
@@ -7,6 +7,13 @@ type ResError struct {
 	Message string
 }
 
+func NewResError(code *int, message string) *ResError {
+	return &ResError{
+		Code:    code,
+		Message: message,
+	}
+}
+
 // Implement the Error method for the MyError struct
 func (e *ResError) Error() string {
 	return fmt.Sprintf("Error %d: %s", e.Code, e.Message)
